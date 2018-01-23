df849ec4f7be957a08f67a25c008b91c3d204ac0
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2018-01-23 09:53:48 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -28,11 +28,14 @@ def renderPage1():
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     #set the first and last name in the session
+    session[firstName] = request.form["firstName"]
+    session[lastName] = request.form["lastName"]
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
     #set the favorite color in the session
+    session[favoriteColor] = request.args["favoriteColor"]
     return render_template('page3.html')
     
 if __name__=="__main__":
